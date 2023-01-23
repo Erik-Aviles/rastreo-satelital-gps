a11eb40e9af556ce432d7a9c52f0cd4d5c4f457b
Erika <erik8822@hotmail.com>
2023-01-23 09:56:47 -0500

    [start] "Configuracion del entorno de desarrollo"

---
+++ src/routes/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import  '../styles/App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{color: 'red'}}>App</h1>
+    </div>
+  )
+}
+
+export default App
